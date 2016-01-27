c28d52dc21f24e88129fad97e71bad30249bf182
VictorMuramoto <victor.muramoto@gmail.com>
2016-01-27 21:59:20 -0200

    tests and featured attribute included in job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127113641) do
+ActiveRecord::Schema.define(version: 20160127235420) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160127113641) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "featured"
   end
 
 end
--- spec/features/user/user_create_job_spec.rb
+++ spec/features/user/user_create_job_spec.rb
@@ -13,7 +13,21 @@ feature 'user create a job' do
     fill_in 'Descrição', with: job.description
 
     click_on 'Criar Vaga'
+  end
 
+  scenario 'user can feature a job' do
+    job = new_job
+
+    visit new_job_path
+
+    fill_in 'Título', with: job.title
+    fill_in 'Empresa', with: job.company
+    fill_in 'Categoria', with: job.category
+    fill_in 'Localização', with: job.location
+    fill_in 'Descrição', with: job.description
+    check   'Destaque'
+
+    click_on 'Criar Vaga'
 
   end
 end
