f4f6029a5b311859f1067dc208fd7d1d22b243c4
Allan <allan_klaus@globo.com>
2016-01-28 13:51:20 -0200

    Created user dashboard, only the owner can access the dashboard that contain his companies, jobs, related categories an contract

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,8 @@ class DashboardController < ApplicationController
   def index
     @jobs = Job.joins(company: :user).where(companies:{user_id: current_user.id})
     @companies = current_user.companies
-    @categories = Category.joins(job: :company).where(companies:{user_id: current_user.id})
-    @contracts = Contract.all
+    @categories = Category.joins(jobs: :company).where(companies:{user_id: current_user.id}).group(:id)
+    @contracts = Contract.joins(jobs: :company).where(companies:{user_id: current_user.id}).group(:id)
 
     render 'home/index'
   end
