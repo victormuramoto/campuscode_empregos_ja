2fe2bdd7de7d485e983df3a6fd232668a059809a
Allan <allan_klaus@globo.com>
2016-01-28 16:57:05 -0200

    Refatoring feature 'Visitor view recent jobs'

--- spec/features/visitor/visitor_views_recent_jobs_spec.rb
+++ spec/features/visitor/visitor_views_recent_jobs_spec.rb
@@ -2,17 +2,10 @@ require 'rails_helper'
 
 feature 'Visitor view recent jobs' do
   scenario 'successfully' do
-    company = Company.create(name:     'Campus Code',
-                             location: 'São Paulo',
-                             mail:     'contato@campuscode.com.br',
-                             phone:    '2369-3476')
-
-    category = Category.create(name: 'Desenvolvedor')
-    job = create_job(company, {category: category})
+    job = create_job(create_company)
 
     visit root_path
 
-    # primeira elemento com o css "jobs" da pagina
     within('.jobs:first-child') do
       expect(page).to have_content('Novidade')
     end
