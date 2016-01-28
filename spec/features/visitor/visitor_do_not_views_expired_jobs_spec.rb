035b967db749199727af1d517df8bb7f25067558
Allan <allan_klaus@globo.com>
2016-01-28 16:54:44 -0200

    Refactoring feature 'visitor do not views expired jobs'

--- spec/features/visitor/visitor_do_not_views_expired_jobs_spec.rb
+++ spec/features/visitor/visitor_do_not_views_expired_jobs_spec.rb
@@ -2,59 +2,64 @@ require 'rails_helper'
 
 feature 'Visitor do not view expired jobs' do
   scenario 'successfully show jobs passed  5 days' do
+    job = nil
     travel_to 5.days.ago do
-      create_job()
+      job = create_job
     end
 
     visit root_path
 
-    expect(page).to have_content("Desenvolvedor Rails")
+    expect(page).to have_content job.title
   end
 
   scenario 'successfully show jobs passed  89 days' do
+    job = nil
     travel_to 89.days.ago do
-      create_job()
+      job = create_job
     end
 
     visit root_path
 
-    expect(page).to have_content("Desenvolvedor Rails")
+    expect(page).to have_content job.title
   end
 
   scenario 'successfully show jobs passed  90 days' do
+    job = nil
     travel_to 90.days.ago do
-      create_job()
+      job = create_job
     end
 
     visit root_path
 
-    expect(page).to_not have_content("Desenvolvedor Rails")
+    expect(page).to_not have_content job.title
   end
 
   scenario 'successfully show jobs passed  91 days' do
+    job = nil
     travel_to 91.days.ago do
-      create_job()
+      job = create_job
     end
 
     visit root_path
 
-    expect(page).to_not have_content("Desenvolvedor Rails")
+    expect(page).to_not have_content job.title
   end
 
   scenario 'successfully show jobs passed  100 days' do
+    job = nil
     travel_to 100.days.ago do
-      create_job()
+      job = create_job
     end
 
     visit root_path
 
-    expect(page).to_not have_content("Desenvolvedor Rails")
+    expect(page).to_not have_content job.title
   end
 
   scenario 'show expired job' do
     job = nil
     travel_to 100.days.ago do
-      job = create_job()
+      job = create_job
     end
 
     visit job_path(job)
@@ -63,9 +68,7 @@ feature 'Visitor do not view expired jobs' do
   end
 
   scenario 'show not expired job' do
-    job = create_job()
-
-    visit job_path(job)
+    visit job_path(create_job)
 
     expect(page).to_not have_content("Vaga Expirada!")
   end
