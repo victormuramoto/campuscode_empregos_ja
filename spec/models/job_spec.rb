3d6839f036c5f1c8b660257676f88d6b6db9e3b7
VictorMuramoto <victor.muramoto@gmail.com>
2016-01-29 19:13:19 -0200

    Created tests - Recent jobs

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,1 +1,27 @@
 require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+  describe "#recent?" do
+    
+    context 'created today' do
+      it 'is recent' do
+        expect(create_job).to be_recent
+      end
+    end
+
+    context 'created 4 days ago' do
+      it 'is recent' do
+        expect(create_job).to be_recent
+      end
+    end
+
+    context 'created 6 days ago' do
+      it 'is not recent' do
+        expect(create_job).to_not be_recent
+      end
+    end
+
+
+  end
+
+end
