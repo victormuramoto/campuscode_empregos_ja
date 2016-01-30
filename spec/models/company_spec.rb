4340c8c9810c1bcc726e9d10e5df6af6091c1401
VictorMuramoto <victor.muramoto@gmail.com>
2016-01-29 23:09:22 -0200

    Created tests - Premium companies

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
-# RSpec.describe Company, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Company, type: :model do
+    before do
+      @company  = create_company
+      @category = create_category
+    end
+
+    describe '#premium?' do
+
+      context 'have no job' do
+        it 'is not premium' do
+          expect(@company).to_not be_premium
+        end
+      end
+
+      context 'have 1 job' do
+        it 'is not premium' do
+          1.times do
+            create_job(company:@company,category:@category)
+          end
+          expect(@company).to_not be_premium
+        end
+      end
+
+      context 'have 4 job' do
+        it 'is not premium' do
+          4.times do
+            create_job(company:@company,category:@category)
+          end
+          expect(@company).to_not be_premium
+        end
+      end
+
+      context 'have 5 job' do
+        it 'is premium' do
+          5.times do
+            create_job(company:@company,category:@category)
+          end
+          expect(@company).to_not be_premium
+        end
+      end
+
+      context 'have 6 job' do
+        it 'is premium' do
+          6.times do
+            create_job(company:@company,category:@category)
+          end
+          expect(@company).to_not be_premium
+        end
+      end
+
+    end
+end
