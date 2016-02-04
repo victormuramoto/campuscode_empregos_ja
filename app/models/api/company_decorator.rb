38992a73f386d55602b16a02ac010c2981459597
Elton e Victor <timelocaweb8@campuscode.com.br>
2016-02-04 11:50:00 -0200

    CompanyDecorater have initialize method

--- app/models/api/company_decorator.rb
+++ app/models/api/company_decorator.rb
@@ -2,16 +2,20 @@ module Api
   class CompanyDecorator < SimpleDelegator
     include Rails.application.routes.url_helpers
 
+    def initialize(company)
+      @companyD = company
+    end
+
     def to_json(options = {})
       json.merge(paths).to_json
     end
 
     def json
-      JSON.parse(__getobj__.to_json)
+      JSON.parse(@companyD.to_json)
     end
 
     def paths
-      {jobs_path: jobs_api_company_path(__getobj__)}
+      {jobs_path: jobs_api_company_path(@companyD)}
     end
 
   end
